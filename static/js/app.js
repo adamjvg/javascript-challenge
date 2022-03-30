9f1046ad7e8129c9b27cbea480e1e25d2bf6d5f8
Adam Van Geleuken <89176126+adamjvg@users.noreply.github.com>
2022-03-30 19:15:19 +0800

    added some style changes

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,48 @@
-// from data.js
+// create more explicit variable
 var tableData = data;
 
-// YOUR CODE HERE!
+// create variable to append
+var tbody = d3.select('tbody');
+
+// append objects from data.js into variable
+tableData.forEach(item => {
+    var tr = tbody.append('tr');
+    tr.append('td').text(item.datetime);
+    tr.append('td').text(item.city);
+    tr.append('td').text(item.state);
+    tr.append('td').text(item.country);
+    tr.append('td').text(item.shape);
+    tr.append('td').text(item.durationMinutes);
+    tr.append('td').text(item.comments);
+});
+
+// create button
+var button = d3.select('#filter-btn');
+
+// function to commence when button clicked by user
+button.on('click', function() {
+    
+    // make variables for inputs and values for date
+    var dateInput = d3.select('#date-filter');
+    var dateValue = dateInput.property('value');
+   
+    //filter by date
+    var filtered = tableData.filter(item => item.datetime === dateValue)
+     
+    console.log(filtered);
+    
+    // refresh table
+    tbody.html(``);
+    
+    // add data
+    filtered.forEach( item => {
+        var tr = tbody.append('tr');
+        tr.append('td').text(item.datetime);
+        tr.append('td').text(item.city);
+        tr.append('td').text(item.state);
+        tr.append('td').text(item.country);
+        tr.append('td').text(item.shape);
+        tr.append('td').text(item.durationMinutes);
+        tr.append('td').text(item.comments);
+    });
+});
